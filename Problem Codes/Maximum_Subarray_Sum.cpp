dba4cff5dc0600085030133ed0c95bd8cc87df76
bhanuroc <bhanu.pratap@iitg.ac.in>
2020-08-11 21:16:28 +0530

    Update Maximum_Subarray_Sum.cpp

--- Problem Codes/Maximum_Subarray_Sum.cpp
+++ Problem Codes/Maximum_Subarray_Sum.cpp
@@ -40,4 +40,41 @@ int main() {
 
 
 }
+//Another Solution using dp
+#include<bits/stdc++.h>
+using namespace std;
+#define sz(x) (int)x.size()
+#define pb push_back
+#define mp make_pair
+#define ll long long 
+
+void fast(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+}
+
+
+int main() {
+   fast();
+   int n;
+   cin>>n;
+   long long a[n];
+   long long dp[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+   dp[0]=a[0];
+   for(int  i=1;i<n;i++){
+       dp[i]=max(dp[i-1]+a[i],a[i]);
+   }
+   long long ans=dp[0];
+   for(int i=1;i<n;i++){
+      ans=max(dp[i],ans);
+   }
+   cout<<ans<<"\n";
+
+
+}
+
 // Another Solution Using Kadane's algo
+
+
